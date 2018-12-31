06f44c006c65f40345602b0033fabd0d238138f7
Javier Falces <javifalces@gmail.com>
2018-12-31 10:39:16 +0100

    replacing big files

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     packages=['', 'tradeasystems_connector', 'tradeasystems_connector.conf',
               'tradeasystems_connector.util', 'tradeasystems_connector.model', 'tradeasystems_connector.service'],
     url='',
-    license='OPEN',
+    license='GNU GPL 2.0',
     author='javier_falces',
     author_email='javifalces@gmail.com',
     description='Layer  to download/trade with different sources brokers , persistence in PyStore'
