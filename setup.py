a27c70413c324a781a481831dc42a611b0b653cc
Javier Falces <javifalces@gmail.com>
2018-12-31 10:28:53 +0100

    fix package error

--- setup.py
+++ setup.py
@@ -3,7 +3,9 @@ from setuptools import setup
 setup(
     name='tradeasystems_connector',
     version='1.0.0',
-    packages=['', 'tradeasystems_connector'],
+    packages=['', 'tradeasystems_connector', 'dao', 'conf', 'data', 'util', 'model', 'tests', 'tests.dao',
+              'tests.broker', 'tests.service',
+              'tests.historical_market_data', 'broker', 'service', 'historical_market_data'],
     url='',
     license='',
     author='javier_falces',
