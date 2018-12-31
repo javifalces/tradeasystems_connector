108427280133924593effa08a874511c97f8cd20
Javier Falces <javifalces@gmail.com>
2018-12-31 10:33:08 +0100

    adjust setup package

--- setup.py
+++ setup.py
@@ -3,11 +3,10 @@ from setuptools import setup
 setup(
     name='tradeasystems_connector',
     version='1.0.0',
-    packages=['', 'tradeasystems_connector', 'dao', 'conf', 'data', 'util', 'model', 'tests', 'tests.dao',
-              'tests.broker', 'tests.service',
-              'tests.historical_market_data', 'broker', 'service', 'historical_market_data'],
+    packages=['', 'tradeasystems_connector', 'tradeasystems_connector.conf',
+              'tradeasystems_connector.util', 'tradeasystems_connector.model', 'tradeasystems_connector.service'],
     url='',
-    license='',
+    license='OPEN',
     author='javier_falces',
     author_email='javifalces@gmail.com',
     description='Layer  to download/trade with different sources brokers , persistence in PyStore'
